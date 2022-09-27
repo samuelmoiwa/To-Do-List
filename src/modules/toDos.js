0113ec4105866acc35acea5a993f51938240011f
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-09-27 23:52:30 +0000

    add toDos.json

---
+++ src/modules/toDos.js
@@ -0,0 +1,24 @@
+const tasks = [
+  {
+    description: 'Try to love webpack',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Order food',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Eat food',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Go to bed',
+    completed: false,
+    index: 3,
+  },
+];
+
+export default tasks;
