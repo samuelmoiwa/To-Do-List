cb9656008ea7cc8a06878a24c1fe4499dcf29f7a
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-09-28 00:32:00 +0000

    update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-   },
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
       template: './src/index.html',
+
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, './build'),
-   },
-   mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './build'),
+  },
+  mode: 'development',
 
-   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader', 
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
-  {
-    test: /\.(png|svg|jpg|jpeg|gif)$/i,
-    type: 'asset/resource',
-  },
-  {
-    test: /\.css$/i,
-    use: ['style-loader', 'css-loader'],
-  }
-],
-  },
-    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
- };
+};
