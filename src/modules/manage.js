3b5dae24dcec7ff9d285ace22fd8e334eee086c8
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-06 18:52:57 +0000

    update manage.js

--- src/modules/manage.js
+++ src/modules/manage.js
@@ -1,10 +1,6 @@
-import removeIcon from '../icons/remove.png';
-
-const icon3 = new Image();
-icon3.src = removeIcon;
 const toDos = [];
 
-export default class ToDo {
+export default class ClassToDo {
   constructor(todo) {
     this.description = todo;
     this.index = toDos.length + 1;
@@ -12,7 +8,7 @@ export default class ToDo {
     this.completed = false;
   }
 
-  static add() {
+  static addItems() {
     const alert = document.querySelector('.alert');
     const addIcon = document.getElementById('add-icon');
     const doInput = document.getElementById('do-input');
@@ -21,7 +17,7 @@ export default class ToDo {
       let toDos = [];
       toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
       if (doInput.value !== '') {
-        const newTask = new ToDo(doInput.value);
+        const newTask = new ClassToDo(doInput.value);
         toDos.push(newTask);
         toDos.forEach((task, i) => {
           task.index = i + 1;
@@ -32,71 +28,6 @@ export default class ToDo {
       } else {
         alert.innerText = 'input in empty, add text!';
         alert.style.display = 'flex';
-      }
-    });
-  }
-
-  static remove() {
-    const removeIcon = document.querySelectorAll('.remove');
-    removeIcon.forEach((icon) => {
-      icon.addEventListener('click', (e) => {
-        let toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
-        toDos = toDos.filter((task) => task.id !== e.target.id);
-        toDos.forEach((task, i) => {
-          task.index = i + 1;
-        });
-        localStorage.setItem('toDos', JSON.stringify(toDos));
-        window.location.reload();
-      });
-    });
-  }
-
-  static edit() {
-    const taskItems = document.querySelectorAll('.task-item');
-    taskItems.forEach((item) => {
-      item.addEventListener('click', () => {
-        item.style.background = '#ddd';
-        item.setAttribute('contenteditable', 'true');
-      });
-      item.addEventListener('focusout', () => {
-        const toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
-        item.style.background = 'none';
-        toDos.forEach((task) => {
-          if (task.index.toString() === item.id) {
-            task.description = item.value;
-            localStorage.setItem('toDos', JSON.stringify(toDos));
-          }
-        });
-      });
-    });
-  }
-
-  static clear() {
-    const clearButton = document.getElementById('clear');
-    clearButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      let toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
-      toDos = toDos.filter((task) => task.completed === false);
-      localStorage.setItem('toDos', JSON.stringify(toDos));
-      window.location.reload();
-    });
-  }
-
-  static reset() {
-    const resetButton = document.querySelector('.reset');
-    const alert = document.querySelector('.alert');
-    let toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
-    resetButton.addEventListener('click', () => {
-      if (toDos === '') {
-        alert.innerText = 'List is empty.';
-        alert.style.display = 'flex';
-      } else {
-        // eslint-disable-next-line no-lonely-if, no-restricted-globals, no-alert
-        if (confirm('All items will be remove perminately. Are you sure?')) {
-          toDos = [];
-          localStorage.setItem('toDos', JSON.stringify(toDos));
-          window.location.reload();
-        }
       }
     });
   }
