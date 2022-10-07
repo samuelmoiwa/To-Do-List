45d3d603abdbf032ef2a30cb1c3b5413f6a2332a
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-07 06:34:23 +0000

    update resetList.js

--- src/modules/resetList.js
+++ src/modules/resetList.js
@@ -1,18 +1,18 @@
 export default function reset() {
   const resetButton = document.querySelector('.reset');
-  const alert = document.querySelector('.alert');
-  let toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
+  const alert = document.querySelector('.alertText');
+  let toDoItems = JSON.parse(localStorage.getItem('toDos') || '[]');
   resetButton.addEventListener('click', () => {
-    if (toDos === '') {
+    // eslint-disable-next-line eqeqeq
+    if (toDoItems == '') {
       alert.innerText = 'List is empty.';
       alert.style.display = 'flex';
     } else {
-      // eslint-disable-next-line no-lonely-if, no-restricted-globals, no-alert
-      if (confirm('All items will be remove perminately. Are you sure?')) {
-        toDos = [];
-        localStorage.setItem('toDos', JSON.stringify(toDos));
-        window.location.reload();
-      }
+      // eslint-disable-next-line no-restricted-globals, no-alert
+      confirm('All items will be remove perminately. Are you sure?');
+      toDoItems = [];
+      localStorage.setItem('toDos', JSON.stringify(toDoItems));
+      window.location.reload();
     }
   });
 }
