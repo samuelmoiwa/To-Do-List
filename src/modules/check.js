77acf7c483c19d2bcf616c5b86adcf5e359f63c0
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-07 06:33:18 +0000

    update check.js

--- src/modules/check.js
+++ src/modules/check.js
@@ -1,18 +1,19 @@
-export default function check() {
+export default function checkBox() {
   const inputChecks = document.querySelectorAll('.check');
   inputChecks.forEach((check) => {
     check.addEventListener('change', (e) => {
-      let toDos = [];
-      toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
-      toDos.forEach((task) => {
+      let toDoItems = [];
+      toDoItems = JSON.parse(localStorage.getItem('toDos') || '[]');
+
+      toDoItems.forEach((task) => {
         if ((e.target.id === `input${task.index}`) && (task.completed === false)) {
           task.completed = true;
           e.target.checked = true;
-          localStorage.setItem('toDos', JSON.stringify(toDos));
+          localStorage.setItem('toDos', JSON.stringify(toDoItems));
         } else if ((e.target.id === `input${task.index}`) && (task.completed === true)) {
           task.completed = false;
           e.target.checked = false;
-          localStorage.setItem('toDos', JSON.stringify(toDos));
+          localStorage.setItem('toDos', JSON.stringify(toDoItems));
         }
       });
     });
